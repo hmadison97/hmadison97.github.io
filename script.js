d2e11eb1d5d15e1612604b033973c8338dffdb13
geomin76 <geom@vt.edu>
2020-12-14 18:22:11 -0500

    Deleting old code

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-    // "use strict";
-
-    // let map;
-
-    // function initMap() {
-    //   map = new google.maps.Map(document.getElementById("map"), {
-    //     center: {
-    //       lat: -34.397,
-    //       lng: 150.644
-    //     },
-    //     zoom: 8,
-    //     gestureHandling: 'greedy'
-    //   });
-    // }
-
-
-
     var map;
     var infoWindow;
 
