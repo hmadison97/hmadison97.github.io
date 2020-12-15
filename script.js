194b0c9d351c33cdfbb75cfa7c19750600278691
hmadison97 <69611328+hmadison97@users.noreply.github.com>
2020-12-15 00:30:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -580,8 +580,8 @@
         <div style="margin: auto; text-align: center">
           <p>Oregon</p>
           <img width="100%" src="https://lh3.googleusercontent.com/gsX_TqcQ2NAV3dNNC-tzcUWq_wdGovzjPlL-uR8POLvKKTL4vQjyxgRymj6ByuCo0OshVQ73veAnoUeMg8wtbnUVGlkuABuHKHDBJPURga4qoy6BKn-4-QuPWRtO-U7cuoOo8tPLgjE=w2400">
-          <img width="100%" src="https://lh3.googleusercontent.com/2Fz6Fn5zq_hh75oNLsyNqyGSHzPopHojN77Eu6GImw_3bb4JteONR_K8lnCY2nRbZQV9RD7ACVYvTHEEoW6oGt2GNkAVXzsGdHl1XI9JWwr9ojo3N7t5mYgqaux8lESdvi4mJTti4Ok=w2400">
-          <img width="100%" src="https://lh3.googleusercontent.com/2Fz6Fn5zq_hh75oNLsyNqyGSHzPopHojN77Eu6GImw_3bb4JteONR_K8lnCY2nRbZQV9RD7ACVYvTHEEoW6oGt2GNkAVXzsGdHl1XI9JWwr9ojo3N7t5mYgqaux8lESdvi4mJTti4Ok=w2400">
+          <img width="100%" src="https://lh3.googleusercontent.com/DDaF_NQnW2l9WFRCoZd-LL3g6kl0HQo8OQgeb67hnxnzcoHRgO9ZWIfq_tfVGehGZRXV9dRK6pYf_ONvziNTUFMlFFmGeTOvOmTW3vnSZzDZG9pGAsnFOcxc3RjAwx3oL-OKy32Qffs=w2400">
+          <img width="100%" src="https://lh3.googleusercontent.com/DDaF_NQnW2l9WFRCoZd-LL3g6kl0HQo8OQgeb67hnxnzcoHRgO9ZWIfq_tfVGehGZRXV9dRK6pYf_ONvziNTUFMlFFmGeTOvOmTW3vnSZzDZG9pGAsnFOcxc3RjAwx3oL-OKy32Qffs=w2400">
           <img width="100%" src="https://lh3.googleusercontent.com/w0I95i7hDc3G2u_Os_nVfZeSyM89AeHD7mEpTvDb_58LhPA1IdsveeqX-IMbDRhJBWbWrlO9sUq0Go6ODvtkCAFgb7a-D5CqnLuLWrsAYidhFYxa55F2X9oXWpj57OJJeLOayWuWH1E=w2400">
           <img width="100%" src="https://lh3.googleusercontent.com/Ok2fDsGF1nUd2dLYDK57FuA7UH1liGttaGgSatJ_m-PS6qKMG5yFmep2JkwIO2YJUE3a_cRFwvaIHcTwImb4jahBaF8-nRbtFlwSQUdeUkrDj2ZOZalbzingMKlgjTi_nU7Kx48q9pk=w2400">
           <img width="100%" src="https://lh3.googleusercontent.com/f1qUBpwHlhXoYL2iNszu36HKc9Orei6tjSi6PiIhYXhFTWkNdIIeMRfB9G0E_-ex2TIS791t59n2ZCbKleBfwXSumBNv-kSm_P9W9J7i1YsDm69IJ5WCxVwZnZ_UmocPUZ39DM_MvM8=w2400">
